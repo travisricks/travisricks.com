55f2be463f517332061bca680c1101769d59cf6c
Travis Ricks <travis@travisricks.com>
2020-03-25 17:39:49 +0900

    update website with projects after CC

--- js/resume.js
+++ js/resume.js
@@ -1,15 +1,23 @@
 (function($) {
-  "use strict"; // Start of use strict
+  // Start of use strict
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      let target = $(this.hash);
+      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          'easeInOutExpo'
+        );
         return false;
       }
     }
@@ -22,7 +30,6 @@
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#sideNav'
+    target: '#sideNav',
   });
-
 })(jQuery); // End of use strict
